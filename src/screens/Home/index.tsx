a3b2443ce6bf04d6b5e0ffae8a3e502fb97ae201
Saymon Araújo <67705166+saymon-araujo@users.noreply.github.com>
2021-10-20 07:25:43 -0300

    chore/style: import assets and create main header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,25 @@
 import React from "react";
-import { Container, Title } from "./styles";
+import { Container, Header, TotalCars, HeaderContent } from "./styles";
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+import Logo from "../../assets/logo.svg";
 
 export function Home() {
   return (
     <Container>
-      <Title>Home</Title>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
     </Container>
   );
 }
